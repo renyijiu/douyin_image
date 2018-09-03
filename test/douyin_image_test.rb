7e8ba319e70d0c735327d87a8b9ea556b6e30d20
renyijiu <lanyuejin1108@gmail.com>
2018-09-03 17:34:52 +0800

    [test]完善测试代码

--- test/douyin_image_test.rb
+++ test/douyin_image_test.rb
@@ -5,7 +5,87 @@ class DouyinImageTest < Minitest::Test
     refute_nil ::DouyinImage::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_raise_when_no_input_image
+    assert_raises do
+      DouyinImage.generate('')
+    end
   end
+
+  def test_generate_when_no_output_path
+    DouyinImage.generate(TEST_FILE)
+
+    assert File.file?(OUTPUT_FILE)
+    File.delete(OUTPUT_FILE)
+  end
+
+  def test_generate_when_has_output_path
+    DouyinImage.generate(TEST_FILE, './tmp/')
+
+    assert File.file?(OUTPUT_FILE)
+    File.delete(OUTPUT_FILE)
+  end
+
+  def test_generate_when_has_offset
+    DouyinImage.generate(TEST_FILE, nil, {offset: 10})
+
+    assert File.file?(OUTPUT_FILE)
+    File.delete(OUTPUT_FILE)
+  end
+
+  def test_generate_when_has_negative_offset
+    DouyinImage.generate(TEST_FILE, nil, {offset: -10})
+
+    assert File.file?(OUTPUT_FILE)
+    File.delete(OUTPUT_FILE)
+  end
+
+  def test_generate_when_has_part_count
+    DouyinImage.generate(TEST_FILE, nil, {part_count: 0})
+
+    assert File.file?(OUTPUT_FILE)
+    File.delete(OUTPUT_FILE)
+  end
+
+  def test_generate_when_has_negative_part_count
+    DouyinImage.generate(TEST_FILE, nil, {part_count: -10})
+
+    assert File.file?(OUTPUT_FILE)
+    File.delete(OUTPUT_FILE)
+  end
+
+  def test_generate_when_has_part_width
+    DouyinImage.generate(TEST_FILE, nil, {part_width: 10})
+
+    assert File.file?(OUTPUT_FILE)
+    File.delete(OUTPUT_FILE)
+  end
+
+  def test_generate_when_has_negative_part_width
+    DouyinImage.generate(TEST_FILE, nil, {part_width: -10})
+
+    assert File.file?(OUTPUT_FILE)
+    File.delete(OUTPUT_FILE)
+  end
+
+  def test_generate_when_has_part_height
+    DouyinImage.generate(TEST_FILE, nil, {part_height: 10})
+
+    assert File.file?(OUTPUT_FILE)
+    File.delete(OUTPUT_FILE)
+  end
+
+  def test_generate_when_has_negative_part_height
+    DouyinImage.generate(TEST_FILE, nil, {part_height: -10})
+
+    assert File.file?(OUTPUT_FILE)
+    File.delete(OUTPUT_FILE)
+  end
+
+  def test_generate_success
+    DouyinImage.generate(TEST_FILE, nil, { part_height: 10, part_count: 10, part_width: 10, offset: 10})
+
+    assert File.file?(OUTPUT_FILE)
+    File.delete(OUTPUT_FILE)
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,6 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "douyin_image"
 
 require "minitest/autorun"
+
+TEST_FILE = './tmp/test.jpg'
+OUTPUT_FILE = './tmp/test_douyin.jpg'
